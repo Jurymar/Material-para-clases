ac223ac40e737b8ff98b4f5e19a7b0a96fdf3514
Jurymar Colmenares <jurymarcolmenares@gmail.com>
2025-12-11 19:04:08 -0500

    no message

--- Clases.playground/Pages/2 Variables, constantes, tipos y optionals.xcplaygroundpage/Contents.swift
+++ Clases.playground/Pages/2 Variables, constantes, tipos y optionals.xcplaygroundpage/Contents.swift
@@ -2,6 +2,42 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//⭐VARIABLES
 
-//: [Next](@next)
+//las variables tambien son conocidas como atributos o propiedades
+//son espacios de almacenemiento, podemos decir que son como una caja de herramienta, se dividen en 2
+
+//✅ 1. Variables (var): pueden cambiar su valor mientras la app está funcionando, osea son mutables
+
+var saludo: String = "Hello"
+saludo = "Hi"
+print(saludo)
+
+//Con inferencia de tipo
+var saludo1 = "Hello"
+
+/*
+ ¿Cuándo se usan?
+ Cuando un valor va a cambiar:
+ nombre, contador, texto, estados de botones, etc.
+ */
+
+
+//✅ 2. Constantes (let): no pueden cambiar su valor mientras la app está funcionando, osea son no mutables
+
+let pi: Double = 3.14159
+
+/*
+ ¿Cuándo se usan?
+ Cuando sabes que el valor será siempre el mismo durante toda la ejecución.
+ */
+
+
+//⭐ TIPOS DE DATOS
+
+//Los tipos de datos definen que tipo de valor puede almacenar una variable o constante, cada dato que se almacena tiene un tipo: texto, número, verdadero/falso, etc.
+
+//Los tipos de datos inician con mayuscula
+
+
+//Cambiar de tipo
--- Clases.playground/Pages/Varias practicas.xcplaygroundpage/Contents.swift
+++ Clases.playground/Pages/Varias practicas.xcplaygroundpage/Contents.swift
@@ -1,7 +1,72 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+protocol Animal {
+    var numeroPatas: Int { get set} //tengo que indicar que control de acceso debe tener la propiedad del protocolo
+}
 
-let 
+protocol Volador {
+    func volar()
+}
+
+// los struct no se pueden mutar , si quiero que sea mutable debo colocarle el modificador mutating
+struct Perro: Animal {
+    var numeroPatas: Int = 4
+    
+       func correr() { // ya que el struct no se puede modificar debo usar en la funcion el modificador mutating
+    }
+}
+
+//Pero si intento mutar en una clase, si me lo permite
+
+class Gato: Animal {
+    var numeroPatas: Int = 4
+    
+    func patas() {
+    }
+}
+
+struct Paloma: Animal, Volador {
+    var numeroPatas: Int = 2
+    
+    func volar() {
+    }
+}
+
+//En las instancias concretas que estoy creando , si me deja mutar el numero de patas sin que el protocolo me force a que sea mutable osea set
+
+//a diferencia de la class el struct no puede mutar sus atributos si es let , lo debo colocar var
+var perro = Perro()
+print("El perro tiene \(perro.numeroPatas) patas")
+perro.numeroPatas = 6
+
+let paloma = Paloma()
+print("La paloma tiene \(paloma.numeroPatas) patas")
+
+//cuando es una class puedo mutar sus atributos , incluso si la instancia es let
+let gato = Gato()
+print("El gato tiene \(gato.numeroPatas) patas")
+
+gato.numeroPatas = 8
+print("El gato tiene \(gato.numeroPatas) patas")
+
+perro.correr()
+print("El perro tiene \(perro.numeroPatas) patas")
+
+
+//get solo lectura ->
+//get set lectura y escritura -> forza a quien implemente la propiedad sea mutable, si le coloco un let me va a dar error porque no estoy cumpliendo con los requerimientos del set
+
+
+//Animal es un tipo de dato abstracto y los tipos de datos que lo implementan como Perro, Paloma y gato son concretos,
+
+
+//En este caso que se va a modificar el numero de patas de un animal, sin importar cual animal ,el numero de patas debe ser mutable, entonces  necesito que el atributo numeroDePAtas sea set,
+func amputarUnaPata(animal: Animal) {
+    var amputando =  animal
+    amputando.numeroPatas = amputando.numeroPatas - 1
+    print("Listo ya amputamos el numero de patas \(amputando.numeroPatas) ")
+}
+// yo necesito que mi propiedad numeroPatas sea seteable (set) para que me debe modificar el nuemero de patas
+
+amputarUnaPata(animal: perro)
+amputarUnaPata(animal: gato)
